916d6e77bcd4b49be95867392d6f2fcf9fdb43e9
jcho42 <jason.cho.301230@gmail.com>
2020-11-17 17:44:44 -0600

    feat: adds navbar header to layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,6 @@ export default function Home() {
         <Header headerText="Hello Gatsby!" />
         <p>What a world.</p>
         <img src="https://source.unsplash.com/random/400x200" alt="" />
-      </Layout>
-      <Layout>
         <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
         <p>
           What do I like to do? Lots of course but definitely enjoy building
